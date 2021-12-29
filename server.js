8ccbaf41b366178307787b5b928fab2cf2fb7a3d
williamtuominiemi <william.m.tuominiemi@gmail.com>
2021-12-29 21:09:43 +0200

    node API serve texture

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 const port = 3000
 
+var public = path.join(__dirname, 'public');
+app.use(express.static(public));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(public, 'flare.png'))
 })
 
 app.listen(port, () => {
